91165f5ec656965038c3443f7c27a4f1f0db3c65
Sagar Patil <54765422+sagar27012001@users.noreply.github.com>
2025-04-15 12:15:48 +0530

    fenwick tree added

---
+++ standard_python/fenwickTree.py
@@ -0,0 +1,31 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.n = len(nums)
+        self.fen = [0 for _ in range(self.n + 1)]
+        self.nums = nums[:]
+        for ind in range(self.n):
+            self.add(ind + 1, nums[ind])
+
+
+    def add(self, index, value) -> None:
+        while index <= self.n:
+            self.fen[index] += value
+            index += (index & -index)
+
+    def update(self, index: int, val: int) -> None:
+        delta = val - self.nums[index]
+        self.nums[index] = val
+        self.add(index + 1, delta)
+
+    def getSum(self, index) -> int:
+        local_sum = 0
+        while index > 0:
+            local_sum += self.fen[index]
+            index -= (index & -index)
+
+        return local_sum
+
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.getSum(right + 1) - self.getSum(left)
